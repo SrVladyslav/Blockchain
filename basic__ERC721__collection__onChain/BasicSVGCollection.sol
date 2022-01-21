52380aab6e430d3397e7a1862a7c56927f5d257c
Vladyslav <vlama@inf.upv.es>
2022-01-22 00:36:58 +0100

    Readme

--- basic__ERC721__collection__onChain/BasicSVGCollection.sol
+++ basic__ERC721__collection__onChain/BasicSVGCollection.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Change required: './../npm/openzeppelin/' -> '@openzeppelin'
-import "./../npm/openzeppelin/contracts/access/Ownable.sol";
-import "./../npm/openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "./../npm/openzeppelin/contracts/utils/Counters.sol";
+// Change required: './../utils/npm/openzeppelin/' -> '@openzeppelin'
+import "./../utils/npm/openzeppelin/contracts/access/Ownable.sol";
+import "./../utils/npm/openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./../utils/npm/openzeppelin/contracts/utils/Counters.sol";
 import "./../utils/Base64_bytes.sol";
 
 contract Basic_SVG_Collection_On_Chain is ERC721URIStorage {
